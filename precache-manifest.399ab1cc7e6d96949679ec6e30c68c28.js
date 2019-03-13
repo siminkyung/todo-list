859faf8df9593a08454c72f32c2473f176d670c0
siminkyung <poo0112@naver.com>
2019-03-13 13:31:12 +0900

    Updates

---
+++ precache-manifest.399ab1cc7e6d96949679ec6e30c68c28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  },
+  {
+    "revision": "887e5c1ccf594bdadee9",
+    "url": "/todo-list/static/js/main.fbba5fa8.chunk.js"
+  },
+  {
+    "revision": "0e7a7007f520a2a71828",
+    "url": "/todo-list/static/js/2.b5657675.chunk.js"
+  },
+  {
+    "revision": "887e5c1ccf594bdadee9",
+    "url": "/todo-list/static/css/main.b716fe19.chunk.css"
+  },
+  {
+    "revision": "ffbe28e7c9b6f32323fde34095a1f5b0",
+    "url": "/todo-list/index.html"
+  }
+];
